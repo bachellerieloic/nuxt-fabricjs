f8747afdb6c39426c935649f3fc4af06a5119e88
Bachellerie Loic <loicbachellerie@me.com>
2020-07-24 14:22:45 -0400

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-export const state = () => ({
-    list: []
-  })
-  
-  export const mutations = {
-    add (state, text) {
-      state.list.push({
-        text,
-        done: false
-      })
-    },
-    remove (state, { todo }) {
-      state.list.splice(state.list.indexOf(todo), 1)
-    },
-    toggle (state, todo) {
-      todo.done = !todo.done
-    }
-  }
-=======
 export const namespaced = true
 import { getField, updateField } from 'vuex-map-fields';
 
@@ -36,4 +16,3 @@ export const actions = {
 export const getters = {
   getField,
 }
->>>>>>> a1c5380... nuxt fabric js working
